db4fbedb90a12b502c09593914cc48af4232057b
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-18 21:19:56 +0100

    build: cascading middlewares

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ const koa = require("koa");
 const app = new koa();
 
 // add a date method to the context
-app.context.date = Date();
 app.context.userData = {
   first: "Manny",
   last: "Henri",
 };
 
+// log
+app.use(async (ctx, next) => {
+  await next();
+  const responseTime = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${responseTime}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const milisecond = Date.now() - start;
+  ctx.set("X-Response-Time", `${milisecond}ms`);
+});
+
 // response
-app.use((ctx) => {
-  // use the state
-  ctx.state.user = "Kehinde Akeredolu";
-
-  // request object usage
-  let from = ctx.request.method;
-
-  // print name with date
-  ctx.response.body = `Hello ${ctx.state.user} on ${ctx.date}`;
-
-  //   ctx.response.body = ctx.userData;
-  console.log(from);
+app.use(async (ctx) => {
+  ctx.response.body = await ctx.userData;
 });
 
 app.listen(3000);
