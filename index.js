feca0952b9269d28cb34a4deb74a77a142dc51f0
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-19 19:29:15 +0100

    build: post method on posts endpoint

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Koa = require("koa");
 const Router = require("koa-router");
+const bodyParser = require("koa-parser");
 
 const app = new Koa();
 const router = new Router();
 const port = 4000;
+
+app.use(bodyParser());
 
 const posts = [
   {
@@ -17,18 +20,36 @@ const posts = [
     content: "Tosn Iyiola",
   },
   {
-    id: "1",
+    id: "3",
     name: "Java Developer",
     content: "Phillip Ogunye",
   },
 ];
 
-router.get("/", (ctx, next) => {
+router.get("/", (ctx) => {
   ctx.body = "Welcome to Koa Router";
 });
 
-router.get("/posts", (ctx, next) => {
+router.get("/posts", (ctx) => {
   ctx.body = posts;
+});
+
+router.post("/posts", (ctx) => {
+  let { id, name, content } = ctx.request.body;
+
+  switch (undefined) {
+    case id:
+      ctx.throw(400, "id is required field");
+    case name:
+      ctx.throw(400, "name is required field");
+    case content:
+      ctx.throw(400, "content is required field");
+      break;
+    default:
+      posts.push({ id, name, content });
+      ctx.body = posts;
+      break;
+  }
 });
 
 app.use(router.routes()).use(router.allowedMethods());
