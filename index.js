2679add021d72cce944006c8d2d9dd5271b71777
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-19 19:04:05 +0100

    build: create posts endpoint

--- index.js
+++ index.js
@@ -5,8 +5,30 @@ const app = new Koa();
 const router = new Router();
 const port = 4000;
 
+const posts = [
+  {
+    id: "1",
+    name: "PHP Developer",
+    content: "Tobi Adelaja",
+  },
+  {
+    id: "2",
+    name: "Node.js Developer",
+    content: "Tosn Iyiola",
+  },
+  {
+    id: "1",
+    name: "Java Developer",
+    content: "Phillip Ogunye",
+  },
+];
+
 router.get("/", (ctx, next) => {
   ctx.body = "Welcome to Koa Router";
+});
+
+router.get("/posts", (ctx, next) => {
+  ctx.body = posts;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
